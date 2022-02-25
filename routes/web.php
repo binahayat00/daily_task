20953d92e66c43127ece3b940436914499bbd92c
amirreza <amirjoun2@gmail.com>
2022-02-25 19:39:58 +0330

    add migrate

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Auth\RegisterController@register');
+Route::get('login', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'App\Http\Controllers\Auth\LoginController@login');
+Route::post('logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+Route::get('register', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'App\Http\Controllers\Auth\RegisterController@register');
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
